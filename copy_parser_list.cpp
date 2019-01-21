8d48a0a1ea8c1ee56d6eb71e4ee5ee9a820d7859
lagunerio <lagunerio@gmail.com>
2019-01-21 16:31:42 +0100

    Update and rename copy_parser_list.cpp to cpy_parser_list.cpp

--- copy_parser_list.cpp
+++
@@ -1,4 +0,0 @@
-/*
- * filename : copy_parser_list.cpp
- * function : copy the most recently updated product list from parser.
- */
---
+++ cpy_parser_list.cpp
@@ -0,0 +1,6 @@
+/*
+ * filename : cpy_parser_list.cpp
+ * function : copy the most recently updated product list from parser
+ * 
+ * Result File  : parser_cpy.csv
+ */
