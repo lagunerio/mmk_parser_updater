2a8a29752132662ff147e1022b91023a102a39e8
lagunerio <lagunerio@gmail.com>
2019-01-21 16:28:24 +0100

    Create validate_time.cpp
    set functions and conditions

---
+++ validate_time.cpp
@@ -0,0 +1,8 @@
+/*
+ * filename  : validate_time.cpp
+ * function  : compare current time and tag 'modified', get difference is shorter than TIME
+ *
+ * Time format      : YYYY-MM-DD HH:MM:SS
+ * Time Difference  : 30min
+ * Result File : parser_new_update.csv
+ */
