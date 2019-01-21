f64c9d3899ca05b2589160c8d3117a558ce006eb
lagunerio <lagunerio@gmail.com>
2019-01-21 16:49:19 +0100

    Update cpy_parser_list.cpp
    create get_data, mk_cpy
    add existing file handling to main

--- cpy_parser_list.cpp
+++ cpy_parser_list.cpp
@@ -4,3 +4,32 @@
  * 
  * Result File  : parser_cpy.csv
  */
+
+#include <uistream>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+RESULT_FILE = "parser_cpy.csv";
+
+void get_data{
+  //get parser data
+  return item_list;
+}
+
+void mk_cpy(){
+  //copy item_list to copy_list
+}
+
+int main(){
+ std::string function;
+ std::string input;
+  
+ if(boost::filesystem::exists(RESULT_FILE)){
+   if(!boost::filesystem::remove(RESULT_FILE)){
+     //remove error handle
+   }
+ }
+  
+}
+
